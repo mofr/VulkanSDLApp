3496cdcc59a4452e0f2ffc6dd416f07487dc87a7
Alexander Egorov <mofr@zond.org>
2024-10-20 12:56:06 +0200

    Extract 3rdparty libraries to a separate directory

--- main.cpp
+++ main.cpp
@@ -12,10 +12,7 @@
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 
-#define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
-
-#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 struct Vertex {
