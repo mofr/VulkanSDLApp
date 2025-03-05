0109c4e344404431ce1c0feafbe9a21de3908dd3
Alexander Egorov <mofr@zond.org>
2025-03-05 21:44:53 +0100

    Split into files and describe coordinate systems

---
+++ Model.h
@@ -0,0 +1,8 @@
+//
+// Created by Alexander on 3.11.24..
+//
+
+#ifndef MODEL_H
+#define MODEL_H
+
+#endif //MODEL_H
---
+++ ObjFile.h
@@ -0,0 +1,8 @@
+//
+// Created by Alexander on 3.11.24..
+//
+
+#ifndef OBJFILE_H
+#define OBJFILE_H
+
+#endif //OBJFILE_H
